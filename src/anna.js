e2ef2ca80f911843654ad2540028d80d5a9a2548
pnm563 <21175423+pnm563@users.noreply.github.com>
2020-09-27 23:58:17 +0100

    Utilities working

--- src/anna.js
+++ src/anna.js
@@ -8,6 +8,11 @@ var cors = require('cors')
 
 var path = "/thing"
 
+
+AWS.config.update({ region: 'eu-west-2' });
+
+let tableName = 'CountryFlags';
+
 var app = express()
 app.use(bodyParser.json())
 app.use(awsServerlessExpressMiddleware.eventContext())
@@ -16,5 +21,34 @@ app.use(cors())
 app.post(path, (req, res) => {
     res.json(req.apiGateway.event);
 })
+
+app.get('/test', (req,res) =>{
+    res.json({"Thanks for that":"test"});
+})
+
+app.get('items', (req, res) => {
+    let queryParams = {
+      TableName: tableName,
+      KeyConditionExpression: "#flagType = :flag",
+      ExpressionAttributeNames:
+      {
+        "#flagType": "FlagType"
+      },
+      ExpressionAttributeValues:
+      {
+        ":flag": "CountryFlag"
+      },
+  };
+  
+    dynamodb.query(queryParams, (err, data) => {
+      if (err) {
+        res.statusCode = 500;
+        res.json({error: 'Could not load items: ' + err});
+      } else {
+        res.json({items: data.Items});
+        //res.json(req.apiGateway.event)
+      }
+    });
+  })
 
 module.exports = app
